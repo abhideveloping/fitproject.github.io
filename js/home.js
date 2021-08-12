0356dafa0e14df6b05eccb5953515d7dd83a64bd
abhideveloping <45909234+abhideveloping@users.noreply.github.com>
2021-08-12 15:02:59 +0530

    fitproject_creation

---
+++ js/home.js
@@ -0,0 +1,13 @@
+console.log("welcome to my website");
+
+gsap.from('.head1', {duration: 2, opacity: 0, delay:1})
+
+gsap.from('.head2', {duration: 1, delay: 2, x: '-120%'})
+
+gsap.from('.head3', {duration: 1, delay: 2, x: '120%'})
+
+
+
+
+
+/* Gsap animation ended over here */
